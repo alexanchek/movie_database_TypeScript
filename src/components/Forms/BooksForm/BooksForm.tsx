b1f1f7ac1dee1cba5482d719bba48064cf8f277d
alexanchek <58663068+alexanchek@users.noreply.github.com>
2021-11-04 15:12:08 +0300

    refactor: removed useless lines from books form

--- src/components/Forms/BooksForm/BooksForm.tsx
+++ src/components/Forms/BooksForm/BooksForm.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
-import { Box, Button, Grid, TextField } from '@mui/material';
+import { Box, Button, Grid } from '@mui/material';
 
 import { useDispatch } from 'react-redux';
 import { getbooks } from '../../../store/actions/dataBookActions';
 
 import { useFormik } from 'formik';
-import * as yup from 'yup';
-import { ERROR_ONLY_LETTERS, ERROR_ONLY_NUMBERS } from '../../../services/errors/errorMessages';
+
 import AppTextField from '../Fields/AppTextField';
 import { formFields } from './BooksFormConfig';
-
-const validationSchema = yup.object({
-    year: yup.string().min(4, 'Это год, только 4 цифры :)').max(4, 'Это год, только 4 цифры :)').matches(/^[0-9]+$/iu, ERROR_ONLY_NUMBERS),
-    genre: yup.string().max(15, 'Слишком много символов').matches(/^[а-яА-Я]+$/iu, ERROR_ONLY_LETTERS),
-    country: yup.string().max(15, 'Слишком много символов').matches(/^[а-яА-Я]+$/iu, ERROR_ONLY_LETTERS)
-})
+import { validationSchema } from '../MoviesForm/MoviesFormConfig';
 
 const MoviesForm = () => {
     const dispatch = useDispatch();
@@ -42,50 +36,6 @@ const MoviesForm = () => {
                         </Grid>
                     )
                 })}     
-                {/* <Grid item xs={12} sm={6}>
-                    <TextField
-                    name="author"
-                    fullWidth
-                    id="year"
-                    label="Введите автора униги"
-                    autoFocus
-                    value={formik.values.year}
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    error={formik.touched.year && Boolean(formik.errors.year)}
-                    helperText={formik.touched.year && formik.errors.year}
-
-                    />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <TextField
-                    name="year"
-                    fullWidth
-                    id="year"
-                    label="Введите год книги"
-                    autoFocus
-                    value={formik.values.year}
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    error={formik.touched.year && Boolean(formik.errors.year)}
-                    helperText={formik.touched.year && formik.errors.year}
-
-                    />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <TextField
-                    type="text"
-                    fullWidth
-                    id="genre"
-                    label="Введите жанр"
-                    name="genre"
-                    value={formik.values.genre}
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    error={formik.touched.genre && Boolean(formik.errors.genre)}
-                    helperText={formik.touched.genre && formik.errors.genre}
-                    />
-                </Grid> */}
             </Grid>
 
             <Button
--- src/components/Forms/BooksForm/BooksFormConfig.ts
+++ src/components/Forms/BooksForm/BooksFormConfig.ts
@@ -1,8 +1,16 @@
 import { selectGenreFields } from "../../../utils/ENVs";
 import { IFormFields } from "../MoviesForm/MoviesFormConfig";
+import * as yup from 'yup';
+import { ERROR_ONLY_LETTERS, ERROR_ONLY_NUMBERS } from '../../../services/errors/errorMessages';
 
 export const formFields: IFormFields[] =  [
     {name: 'Автор', label: 'Введите страну', type: 'textfield'},
     {name: 'genre', label: 'Введите жанр', type: 'selectfield', subfieldsName: 'Жанр', subfields: selectGenreFields},
     {name: 'year', label: 'Введите год', type: 'textfield'},
 ];
+
+export const validationSchema = yup.object({
+    year: yup.string().min(4, 'Это год, только 4 цифры :)').max(4, 'Это год, только 4 цифры :)').matches(/^[0-9]+$/iu, ERROR_ONLY_NUMBERS),
+    genre: yup.string().max(15, 'Слишком много символов').matches(/^[а-яА-Я]+$/iu, ERROR_ONLY_LETTERS),
+    country: yup.string().max(15, 'Слишком много символов').matches(/^[а-яА-Я]+$/iu, ERROR_ONLY_LETTERS)
+})
